5a6a67347282512e9bb4ad539d52b90ffd1ddbc2
Robyarta Haruli Ruci <robyarta@bukalapak.com>
2026-01-17 18:51:06 +0700

    add entry point

--- bin/generator.dart
+++
@@ -1,7 +0,0 @@
-import 'package:mangastash_extension/mangastash_extension.dart';
-
-void main() {
-
-  final source = AsuraScanSource();
-
-}
---
+++ bin/mangaclash.dart
@@ -0,0 +1,4 @@
+import 'package:entity_manga_external/entity_manga_external.dart';
+import 'package:mangastash_extension/mangastash_extension.dart';
+
+SourceExternal main() => MangaClashSource();
