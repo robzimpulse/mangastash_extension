d92c6ebbd4bd4248ba2d08de118de04d98b05fac
Robyarta <robyarta.haruli@allofresh.id>
2026-03-09 16:08:38 +0700

    adjust dependency using melos

--- bin/asurascan.dart
+++
@@ -1,4 +0,0 @@
-import 'package:entity_manga_external/entity_manga_external.dart';
-import 'package:mangastash_extension/mangastash_extension.dart';
-
-SourceExternal main() => AsuraScanSource();
--- lib/mangastash_extension.dart
+++
@@ -1,7 +0,0 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
-library;
-
-export 'src/asurascan/asura_scan_source.dart';
-export 'src/mangaclash/manga_clash_source.dart';
